b8550df16543ce5c9322943e21eda910c7f1ca71
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-03-05 17:39:25 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartGame();
+            PlayGame();
+            EndGame();
+        }
+
+        private static void StartGame()
+        {
+            Console.WriteLine("Starting game...");
+            AskForUsersName();
+        }
+
+        static void AskForUsersName()
+        {
+            Console.WriteLine("Please enter your name...");
+        }
+
+        private static void PlayGame()
+        {
+            Console.WriteLine("Playing the game...");
+            DisplayMaskedWord();
+            AskForLetter();
+        }
+
+        static void AskForLetter()
+        {
+            Console.WriteLine("Asking for letter...");
+        }
+
+        static void DisplayMaskedWord()
+        {
+            
+            Console.WriteLine("Displaying masked word...");
+        }
+
+        private static void EndGame()
+        {
+            Console.WriteLine("END GAME!");
+        }
+
+    }
+}
